a749c5ae790dc95944bc37579575588f2e275770
MarioAguilarGIT <m.aguilar.blasquez@gmail.com>
2023-03-02 15:11:27 -0600

    added ChromeDrive, 2nd commit

--- src/test/java/Test/FormTest.java
+++ src/test/java/Test/FormTest.java
@@ -1,9 +1,14 @@
 package Test;
 import Steps.FormSteps;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FormTest {
 
     public void SubmitForm(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/form");
+
         FormSteps actions = new FormSteps();
 
         actions.enterFirstName();
